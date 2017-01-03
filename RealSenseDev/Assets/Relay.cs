21867e180f9860ff6a7e57c21ebe486604f0db7f
zr <1249264296@qq.com>
2017-01-03 17:57:07 +0800

    初次提交：插件导入，滑动条效果实现

---
+++ RealSenseDev/Assets/Relay.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Relay : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject sliderObj;
+    private Slider sliderScripts;
+
+    [SerializeField]
+    private GameObject rightHandsRS;
+    private TrackingAction rightHandTracking;
+    private Vector3 rightHandInitPos;
+    private Vector3 rightHandRunPos;
+    private float rightHandHorzScale;
+
+
+
+    void Start()
+    {
+        sliderScripts = sliderObj.GetComponent<Slider>();
+        sliderScripts.value = 0.0f;
+
+        rightHandTracking = rightHandsRS.GetComponent<TrackingAction>();
+        rightHandHorzScale = rightHandTracking.VirtualWorldBoxDimensions.x;
+        rightHandInitPos = rightHandsRS.transform.position;
+        rightHandRunPos = new Vector3(0.0f, 0.0f, 0.0f);
+    }
+    void Update()
+    {
+        rightHandRunPos = rightHandsRS.transform.position;
+        sliderScripts.value = 0.5f + ((rightHandRunPos.x - rightHandInitPos.x) / rightHandHorzScale);
+    }
+
+}
